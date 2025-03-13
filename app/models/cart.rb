3eac96246e02a9ea0e94665eadcf78a0c1925e2b
SunSof <buketovasofi@gmail.com>
2025-03-13 18:53:11 +0100

    set associations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,11 @@
 class Cart < ApplicationRecord
+  has_many :cart_items 
+  has_many :products, through: :cart_items
+  
+  enum :status, {
+    pending: 0,
+    processing: 1,
+    completed: 2,
+    cancelled: 3
+  }, default: :pending
 end
