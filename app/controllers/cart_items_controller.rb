a4af948cbeaf7bdda3c7d4fbf84a0d0efb3e3d72
SunSof <buketovasofi@gmail.com>
2025-03-14 14:36:31 +0100

    fix summary

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,6 +4,8 @@ class CartItemsController < ApplicationController
     @cart_item = CartItem.find(params[:id])
     quantity = params[:quantity].to_i
     quantity = [1, quantity].max
+
+    @cart = current_cart.reload
     
     @cart_item.update(quantity: quantity)
     respond_to do |format|
@@ -11,7 +13,7 @@ class CartItemsController < ApplicationController
       format.turbo_stream do
         render turbo_stream: [
           turbo_stream.replace("cart_item_#{@cart_item.id}", partial: "cart_items/cart_item", locals: { cart_item: @cart_item }),
-          turbo_stream.replace("cart_summary", partial: "carts/summary", locals: { cart: current_cart  })
+          turbo_stream.replace("cart_summary", partial: "carts/summary", locals: { cart: @cart })
         ]
       end
     end
@@ -20,13 +22,15 @@ class CartItemsController < ApplicationController
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
+    
+    @cart = current_cart.reload
 
     respond_to do |format|
-      format.html { redirect_to cart_path, notice: "Товар удален из корзины." }
+      format.html { redirect_to cart_path }
       format.turbo_stream do
         render turbo_stream: [
           turbo_stream.remove(@cart_item),
-          turbo_stream.replace("carts", partial: "carts/summary")
+          turbo_stream.replace("cart_summary", partial: "carts/summary", locals: { cart: @cart })
         ]
       end
     end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
   
   def show 
-    @cart_items = current_cart.cart_items.includes(:product)
+    @cart_items = current_cart.cart_items.includes(product: { image_attachment: :blob })
   end 
 
   def clear
