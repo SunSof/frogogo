86b8a4d87880117f635c8160b48b0e3c455ded52
SunSof <buketovasofi@gmail.com>
2025-03-14 16:19:08 +0100

    create new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,22 @@
 products = Product.create([
-  { name: 'Laptop', price: 999.99, description: 'High performance laptop' },
-  { name: 'Smartphone', price: 599.99, description: 'Latest model smartphone' },
-  { name: 'Headphones', price: 149.99, description: 'Noise-cancelling headphones' },
-  { name: 'Keyboard', price: 79.99, description: 'Mechanical keyboard' },
-  { name: 'Mouse', price: 29.99, description: 'Wireless mouse' }
+  { name: 'Беспроводная колонка Goodyear Bluetooth Speaker', price: 1600.00, description: 'ABC' },
+  { name: 'Женский домашний костюм Sweet Dreams', price: 800.00, description: 'ABC' },
+  { name: 'Плащ-дождевик SwissOak', price: 400.00, description: 'ABC' },
 ])
 
-image_path = Rails.root.join('app/assets/images/prod.png')
+image_paths = [
+  Rails.root.join('app/assets/images/prod.png'),
+  Rails.root.join('app/assets/images/prod2.png'),
+  Rails.root.join('app/assets/images/prod3.png')
+]
 
-products.each do |product|
-  file = File.open(image_path)
-    
+products.each_with_index do |product, index|
+  path = image_paths[index]
+  file = File.open(path)
   product.image.attach(
-    io: file,
-    filename: 'prod.png',
-    content_type: 'image/png'
-  )
+      io: file,
+      filename: File.basename(path),
+      content_type: 'image/png'
+    )
   file.close
 end
